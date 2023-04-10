3c79aa0199fdb3fd40fdda745cd863b3ca56c97a
lgirouard <logangirouard@gmail.com>
2023-04-10 16:39:18 -0300

    index additions - added landing page

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ navToggle.addEventListener('click', () => {
         navToggle.setAttribute('aria-expanded', false);
     }
 })
+
+function deferVideo() {
+
+    //defer html5 video loading
+  $("video source").each(function() {
+    var sourceFile = $(this).attr("data-src");
+    $(this).attr("src", sourceFile);
+    var video = this.parentElement;
+    video.load();
+    // uncomment if video is not autoplay
+    //video.play();
+  });
+
+}
+window.onload = deferVideo;
